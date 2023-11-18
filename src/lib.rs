e4bc889689903047a0c2cb1609868fe3155a9110
david.peherstorfer <david.peherstorfer@netnode.ch>
2023-11-18 10:39:55 +0100

    Implement index buffer

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,8 @@ struct State {
     clear_color: wgpu::Color,
     vertex_buffer: wgpu::Buffer,
     num_vertices: u32,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
 }
 
 #[repr(C)]
@@ -57,18 +59,28 @@ impl Vertex {
 
 const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [0.0, 0.5, 0.0],
-        color: [1.0, 0.0, 0.0],
+        position: [-0.0868241, 0.49240386, 0.0],
+        color: [0.5, 0.0, 0.5],
     },
     Vertex {
-        position: [-0.5, -0.5, 0.0],
-        color: [0.0, 1.0, 0.0],
+        position: [-0.49513406, 0.06958647, 0.0],
+        color: [0.5, 0.0, 0.5],
     },
     Vertex {
-        position: [0.5, -0.5, 0.0],
-        color: [0.0, 0.0, 1.0],
+        position: [-0.21918549, -0.44939706, 0.0],
+        color: [0.5, 0.0, 0.5],
+    },
+    Vertex {
+        position: [0.35966998, -0.3473291, 0.0],
+        color: [0.5, 0.0, 0.5],
+    },
+    Vertex {
+        position: [0.44147372, 0.2347359, 0.0],
+        color: [0.5, 0.0, 0.5],
     },
 ];
+
+const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
 
 impl State {
     async fn new(window: Window) -> Self {
@@ -142,6 +154,13 @@ impl State {
             usage: wgpu::BufferUsages::VERTEX,
         });
         let num_vertices = VERTICES.len() as u32;
+
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(INDICES),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+        let num_indices = INDICES.len() as u32;
 
         let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: Some("Shader"),
@@ -256,6 +275,8 @@ impl State {
             clear_color,
             vertex_buffer,
             num_vertices,
+            index_buffer,
+            num_indices,
         }
     }
 
@@ -330,13 +351,15 @@ impl State {
                 timestamp_writes: None,
             });
 
-            if self.use_color {
-                render_pass.set_pipeline(&self.render_pipeline);
-            } else {
-                render_pass.set_pipeline(&self.challenge_render_pipeline);
-            }
+            // Challenge does not work with draw_indexed anymore.
+            // if self.use_color {
+            render_pass.set_pipeline(&self.render_pipeline);
+            // } else {
+            //     render_pass.set_pipeline(&self.challenge_render_pipeline);
+            // }
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.draw(0..self.num_vertices, 0..1);
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
 
         self.queue.submit(iter::once(encoder.finish()));
